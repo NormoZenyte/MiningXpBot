0a5448fddb06d18d83896469bafbeda11e44877e
NormoZenyte <rsnormo@gmail.com>
2025-02-19 14:43:00 +1100

    git commit Discord bot

--- bot.js
+++ bot.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-console.log("DISCORD_TOKEN:", process.env.DISCORD_TOKEN);
-console.log("CHANNEL_ID:", process.env.CHANNEL_ID);
 const { Client, GatewayIntentBits } = require('discord.js');
 const puppeteer = require('puppeteer');
 require('dotenv').config();
